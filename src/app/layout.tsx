e02eb9956fcc2920faa6ebdca8ca6455d6e327d6
EngJao89 <eng_joao@outlook.com>
2025-06-16 01:59:27 -0300

    feat(04as-toast-alert): create alert with toast, for warnings in app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+
 import "./globals.css";
 import { Header } from "@/components/Header";
 
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-100`}
       >
+        <ToastContainer autoClose={1500}/>
         <Header />
         <main className="pt-16"> 
           {children}

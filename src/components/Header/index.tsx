697ad0dce7279f833ad04de682cca780a86e2cda
EngJao89 <eng_joao@outlook.com>
2025-06-16 00:39:29 -0300

    feat(02as-product-page): create products list page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ export function Header() {
         <NavigationMenuList className="flex gap-6">
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
-              <Link href="/docs" className="flex items-center gap-2 px-4 py-2 text-zinc-800 hover:text-blue-500">
+              <Link href="/" className="flex items-center gap-2 px-4 py-2 text-zinc-800 hover:text-blue-500">
                 <Home className="w-4 h-4 text-zinc-800 hover:text-blue-500" />
                 Home
               </Link>
@@ -22,7 +22,7 @@ export function Header() {
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
-              <Link href="/docs" className="flex items-center gap-2 px-4 py-2 text-zinc-800 hover:text-blue-500">
+              <Link href="/products" className="flex items-center gap-2 px-4 py-2 text-zinc-800 hover:text-blue-500">
                 <Package className="w-4 h-4 text-zinc-800 hover:text-blue-500" />
                 Produtos
               </Link>

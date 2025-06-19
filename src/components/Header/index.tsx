7e343ba296880213f9b95b5584ac9a8a554b0aaf
EngJao89 <eng_joao@outlook.com>
2025-06-18 21:17:56 -0300

    feat(06-signup-sheet): create sheet for register new users

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,30 @@
+'use client'
+
+import dynamic from 'next/dynamic';
 import Link from "next/link";
-import { Home, Package, Heart } from "lucide-react";
+
+import { Home, Package, Heart, User } from "lucide-react";
 import {
   NavigationMenu,
   NavigationMenuList,
   NavigationMenuItem,
   NavigationMenuLink,
 } from "../ui/navigation-menu";
+import { Button } from '../ui/button';
+
+const SignUpSheet = dynamic(() => import('../SignUpSheet'), { 
+  ssr: false, 
+  loading: () => ( 
+    <Button variant="ghost" className="text-zinc-100 hover:text-blue-500">
+      <User className="w-8 h-8" />
+    </Button>
+  ),
+});
 
 export function Header() {
   return (
-    <div className="w-full fixed top-0 left-0 glass-nav z-50">
-      <NavigationMenu className="max-w-7xl mx-auto p-4">
+    <div className="w-full fixed flex justify-between items-center top-0 left-0 glass-nav z-50 p-4">
+      <NavigationMenu className="max-w-7xl mx-auto">
         <NavigationMenuList className="flex gap-6">
           <NavigationMenuItem>
             <NavigationMenuLink asChild>
@@ -38,6 +52,8 @@ export function Header() {
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
+
+      <SignUpSheet />
     </div>
   );
 }

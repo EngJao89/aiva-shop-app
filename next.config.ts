b3f93761912a8e348ea08f22687663fa4e34eeec
EngJao89 <eng_joao@outlook.com>
2025-07-17 23:17:18 -0300

    refactor(18as-change-button): change buy button in card

--- next.config.ts
+++ next.config.ts
@@ -70,6 +70,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'm.media-amazon.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'championgearz.co.za'
       }
     ],
   },
